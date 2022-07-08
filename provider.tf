82476a1b2e13d5498dd3b0b9421319250f193ba8
Ritzke <ritzke@goteleport.com>
2022-07-08 05:22:22 -0700

    change versions

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 4.0"
     }
     template = {
       source  = "hashicorp/template"
@@ -11,11 +11,11 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.2.1"
+      version = "~> 3.3.2"
     }
     local = {
       source = "hashicorp/local"
-      version = "~> 2.0.0"
+      version = "~> 2.2.3"
     }
   }
 }
